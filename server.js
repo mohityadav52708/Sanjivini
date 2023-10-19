c0a2371655a797854ac10f043480d8c95b22563a
mohityadav52708 <rgmupdated@gmail.com>
2023-10-19 19:11:55 +0530

    updated

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const path = require("path");
 
-const app = express()
-const port = 3000
 
+const app = express();
+const port = 3000;
+
+
+app.use(express.static(path.join(__dirname,'views')))
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
-app.get('/', (req, res) => {
-  res.render("index");
+app.get('/',(req,res)=>{
+  res.render('index')
 })
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
+app.get("/about", function(req, res){
+  res.render("AboutUs");
+});
+app.get("/healthCare", function(req, res){
+  res.render("health_care");
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
