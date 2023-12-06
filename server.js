bd57d2fe1101648979388bd05aaa43ddab4b2a1b
Mohit Yadav <rgmupdated@gmail.com>
2023-12-06 20:49:47 +0530

    updateded again

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-
 app.use(express.static(path.join(__dirname,'views')))
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
