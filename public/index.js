71bf7d1f01d348afcef899b2f68bd9c942e42e6f
Mohit Yadav <rgmupdated@gmail.com>
2023-12-13 12:39:20 +0530

    commit

--- public/index.js
+++ public/index.js
@@ -10,7 +10,8 @@ const nodemailer = require('nodemailer');
 const randomstring = require('randomstring');
 const crypto = require('crypto');
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 // const PORT = process.env.PORT || 3000;
 
 // Set up EJS as the view engine
